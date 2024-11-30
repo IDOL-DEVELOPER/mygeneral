644adbb8209907471ea047f68c9ebe1e15378ce2
Ideal developer <shambishnoi75@gmail.com>
2024-11-30 21:36:38 +0530

    Re-add src/, composer.json, and composer.lock back to version control

---
+++ src/IdealServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Src;
+
+use Illuminate\Support\ServiceProvider;
+
+class IdealServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+    }
+}
